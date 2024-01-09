a82bb6bb939dc12fe2e90df7b85fd788eabc47e0
Chandlar Lanier <cklanier@outlook.com>
2024-01-09 01:21:24 -0500

    Initial commit, add user and article schemas

---
+++ models/article.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const article = mongoose.Schema({
+  keyword: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: String,
+    required: true
+  },
+  source: {
+    type: String,
+    required: true
+  },
+  link: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        return validator.isURL(v);
+      }
+    }
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        return validator.isURL(v);
+      }
+    }
+  },
+  owner: {
+    select: false
+  }
+});
---
+++ models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const user = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator(v) {
+        return validator.isEmail(v);
+      },
+      message: "You must enter a valid email",
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+});
